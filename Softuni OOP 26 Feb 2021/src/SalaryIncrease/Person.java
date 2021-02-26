b1292022a483cbeeef27cdfce818b15e112aa596
Ivan Danin <67906259+ivandanin@users.noreply.github.com>
2021-02-26 22:27:01 +0200

    Add files via upload

---
+++ Softuni OOP 26 Feb 2021/src/SalaryIncrease/Person.java
@@ -0,0 +1,70 @@
+package SalaryIncrease;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+    private double percentBonus;
+
+    public Person (String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+        this.percentBonus = percentBonus;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getPercentBonus() {
+        return percentBonus;
+    }
+
+    public void setPercentBonus(double percentBonus) {
+        this.percentBonus = percentBonus;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f leva", getFirstName(), getLastName(), getSalary());
+    }
+
+    public void increaseSalary(double bonus) {
+        if (getAge() < 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 200));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 100));
+        }
+    }
+}
