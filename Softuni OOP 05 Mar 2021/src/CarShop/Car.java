a88b74e5e5bddef3863b23dae03d48bf00c3bce8
Ivan Danin <67906259+ivandanin@users.noreply.github.com>
2021-03-06 18:25:39 +0200

    Add files via upload

---
+++ Softuni OOP 05 Mar 2021/src/CarShop/Car.java
@@ -0,0 +1,13 @@
+package CarShop;
+
+import java.io.Serializable;
+
+public interface Car {
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+
+}
